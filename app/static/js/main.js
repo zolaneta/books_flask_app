5c779b477638bf5e5fd626e4673b4863e55a1d2e
zolaneta <zolaneta@yahoo.com>
2016-06-24 12:14:49 -0400

    created main.js and connected it with html files

---
+++ app/static/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bot on 6/24/16.
+ */
+
+
+
+  function fun() {
+  alert("we have fun");
+}
+
+  function thanks(reason){
+                alert("Thank You " +  reason)
+            }
